728ac579993409d0b72d373549b24347f00a7cf4
loicbdev <loicbodolec@msn.com>
2020-10-19 14:37:21 +0200

    react 5

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -2,17 +2,40 @@ import React from 'react';
 import './QuoteCard.css';
 import PropTypes from 'prop-types';
 
-function QuoteCard(props) {
-  return (
-    <figure className="QuoteCard">
-      <img src={props.image} alt={props.character} />
-      <figcaption>
-        <blockquote>{props.quote}</blockquote>
-        <cite>{props.character}</cite>
-      </figcaption>
-    </figure>
-  );
+import React from 'react';
+import './QuoteCard.css';
+
+class QuoteCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: false,
+    };
+  }
+  render() {
+    return (
+      <figure className="QuoteCard">
+        <img src={this.props.image} alt={this.props.character} />
+        <figcaption>
+          <blockquote>{this.props.quote}</blockquote>
+          <p>
+            <cite>{this.props.character}</cite>
+            <span
+              className={this.state.favorite ? 'is-favorite' : ''}
+              onClick={event => {
+                const newFavorite = !this.state.favorite;
+                this.setState({ favorite: newFavorite });
+              }}
+            >
+              &#9733;
+            </span>
+          </p>
+        </figcaption>
+      </figure>
+    );
+  }
 }
+
 
 QuoteCard.propTypes = {
   characterFirstName: PropTypes.string.isRequired,

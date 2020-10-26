abcc502874b2945b22957e4c005addcdb7aeb868
loicbdev <loicbodolec@msn.com>
2020-10-26 16:16:04 +0100

    correction fonction fléchée getNewQuote dans App

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import './App.css';
-import QuoteCard from './components/QuoteCard';
-import axios from 'axios';
-import React, {Component} from 'react';
+import "./App.css";
+import QuoteCard from "./components/QuoteCard";
+import axios from "axios";
+import React, { Component } from "react";
 
-const oneSimpsonQuote = [
+const SimpsonQuote = [
   {
-   "quote": "Shoplifting is a victimless crime, like punching someone in the dark.",
-   "character": "Nelson Muntz",
-   "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
-   "characterDirection": "Left"
+    quote:
+      "By chilling my loins I increase the chances of impregnating my wife.",
+    character: "Apu Nahasapeemapetilon",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629"
   }
-]
+];
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-      this.state = {
-        quote : oneSimpsonQuote[0]
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: SimpsonQuote[0]
+    };
     this.getNewQuote = this.getNewQuote.bind(this);
   }
 
-  getNewQuote(){
-    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
-    .then(response => response.data)
-    .then(data => {
-      console.log(data);
-      this.setState({
-        quote: data[0],
+  getNewQuote = () =>
+    axios
+      .get("https://thesimpsonsquoteapi.glitch.me/quotes")
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          quote: data[0]
+        });
       });
-     });
-  }
 
-  render (){
+  render() {
     return (
       <div className="App">
-        <QuoteCard quote={this.state.quote}/>
-        <button className="btnForQuote" type="button" onClick={this.getNewQuote}>Clic for NEW QUOTE</button>
+        <QuoteCard quote={this.state.quote} />
+        <button
+          className="btnForQuote"
+          type="button"
+          onClick={this.getNewQuote}
+        >
+          Clic for NEW QUOTE
+        </button>
       </div>
     );
   }
